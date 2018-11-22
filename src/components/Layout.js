624f6f48ae7a346564817644cd9d6ccda7216400
Neiva07 <lucasneiva07@gmail.com>
2018-11-21 21:44:35 -0300

    created a form for users in the chat

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import io from 'socket.io-client';
+import {USER_CONNECTED, LOGOUT} from "../Events"
+import LoginForm from './LoginForm'
 
 const socketUrl = "http://localhost:3231"
 
@@ -9,7 +11,8 @@ export default class Layout extends Component {
         super(props);
 
         this.state ={
-            socket: null
+            socket: null,
+            user: null
         }
     }
     componentDidMount() {
@@ -24,11 +27,24 @@ export default class Layout extends Component {
         this.setState({socket})
     }
 
+    setUser = (user) => {
+        const {socket} = this.state;
+        socket.emit(USER_CONNECTED, user);
+        this.setState({user})
+    }
+
+    logout = () => {
+        const {socket} = this.state;
+        socket.emit(LOGOUT);
+        this.setState({user:null})
+    }
+
     render(){
         const {title} = this.props; 
+        const {socket} = this.state;
         return (
             <div className="container">
-                {title}
+                <LoginForm socket={socket} setUser={this.setUser} />    
             </div>
         )
     }
